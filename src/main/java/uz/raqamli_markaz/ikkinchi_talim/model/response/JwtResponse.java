0662aa21015b059b0d0c324b0483ac2bf984e1f2
xurshid_1989 <hersh1409@gmail.com>
2023-06-19 15:50:48 +0500

    init

--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/model/response/JwtResponse.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/model/response/JwtResponse.java
@@ -3,7 +3,6 @@ package uz.raqamli_markaz.ikkinchi_talim.model.response;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uz.raqamli_markaz.ikkinchi_talim.security.SecurityConstant;
 import java.util.List;
 
 @Getter
@@ -14,14 +13,13 @@ public class JwtResponse {
     private String token;
     private Integer userId;
     private String username;
-    private Long token_expire;
     private List<String> roles;
 
     public JwtResponse(Integer userId, String username, String token, List<String> roles) {
+
         this.userId = userId;
         this.username = username;
         this.token = token;
-        this.token_expire = SecurityConstant.TOKEN_EXPIRE_AT;
         this.roles = roles;
     }
 }
--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/service/AuthService.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/service/AuthService.java
@@ -1,27 +1,56 @@
 package uz.raqamli_markaz.ikkinchi_talim.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import uz.raqamli_markaz.ikkinchi_talim.api.iib_api.IIBServiceApi;
 import uz.raqamli_markaz.ikkinchi_talim.api.one_id.OneIdServiceApi;
 import uz.raqamli_markaz.ikkinchi_talim.api.sms_api.SmsServiceApi;
+import uz.raqamli_markaz.ikkinchi_talim.model.request.LoginRequest;
+import uz.raqamli_markaz.ikkinchi_talim.model.response.JwtResponse;
+import uz.raqamli_markaz.ikkinchi_talim.model.response.Result;
 import uz.raqamli_markaz.ikkinchi_talim.repository.AdminEntityRepository;
 import uz.raqamli_markaz.ikkinchi_talim.repository.RoleRepository;
 import uz.raqamli_markaz.ikkinchi_talim.repository.UserRepository;
 import uz.raqamli_markaz.ikkinchi_talim.security.JwtTokenProvider;
+import uz.raqamli_markaz.ikkinchi_talim.security.UserDetailsImpl;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final IIBServiceApi iibServiceApi;
-    private final SmsServiceApi smsServiceApi;
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final OneIdServiceApi oneIdServiceApi;
     private final JwtTokenProvider jwtTokenProvider;
-    private final AdminEntityRepository adminEntityRepository;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("signIn")
+    public ResponseEntity<?> signIn(@RequestBody LoginRequest loginRequest){
+        try {
+            Authentication authenticate = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getPhoneNumber(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authenticate);
+            UserDetailsImpl userDetails = (UserDetailsImpl) authenticate.getPrincipal();
+            String jwtToken = jwtTokenProvider.generateJWTToken(userDetails);
+            List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+            return ResponseEntity.ok(new JwtResponse(
+                    userDetails.getId(),
+                    userDetails.getUsername(),
+                    jwtToken,
+                    roles));
+        } catch (Exception ex) {
+            return ResponseEntity.status(400).body(new Result("Telefon raqam yoki parol hato kiritilgan, " +
+                    "iltimos tekshirib qayta urinib ko'ring", false));
+        }
+    }
 
 }
