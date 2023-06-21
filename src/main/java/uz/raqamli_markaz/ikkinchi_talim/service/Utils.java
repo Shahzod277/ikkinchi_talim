06666d8a95bda535ab86b64ddb928ed0a51ad4b3
xurshid_1989 <hersh1409@gmail.com>
2023-06-21 12:16:48 +0500

    init

--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/service/Utils.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/service/Utils.java
@@ -2,11 +2,14 @@ package uz.raqamli_markaz.ikkinchi_talim.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import uz.raqamli_markaz.ikkinchi_talim.repository.DiplomaInstitutionRepository;
 
 @Component
 @RequiredArgsConstructor
 public class Utils {
 
-    private final
+    private final DiplomaInstitutionRepository diplomaInstitutionRepository;
+
+
 
 }
