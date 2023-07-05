2547e5870e4dd94cfdcb369ad9d9ab5ecfacd8e9
shahzod2777 <shahzodozbekov7@gmail.com>
2023-07-05 17:32:11 +0500

    фф

---
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/api/my_edu/CreateAppRequestMyEdu.java
@@ -0,0 +1,24 @@
+package uz.raqamli_markaz.ikkinchi_talim.api.my_edu;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CreateAppRequestMyEdu {
+
+    @JsonProperty("data")
+    private Object data;
+
+    @JsonProperty("service")
+    private String service = "18ffa877-987f-4d3d-871f-03cb7e0f4638";
+
+    @JsonProperty("external_id")
+    private String externalId;
+
+    @JsonProperty("status")
+    private String status;
+
+
+}
