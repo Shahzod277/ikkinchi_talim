29f0cbf7a453bf14a1dd18db59d09ab09ba84ac9
shahzod2777 <shahzodozbekov7@gmail.com>
2023-07-13 12:28:13 +0500

    фф

--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/repository/ApplicationRepository.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/repository/ApplicationRepository.java
@@ -14,13 +14,13 @@ import java.util.Optional;
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
 
-    @Query(value = " select a.id,u.full_name fullName,k.speciality_name speciality,k.university_name university," +
+    @Query(value = " select a.id id ,u.full_name fullName,k.speciality_name speciality,k.university_name university," +
             " a.modified_date createDate from application a inner join kvota k on k.id = a.kvota_id inner join users u on u.id = a.user_id " +
             " where k.university_code=?1 and a.application_status=?2 ",nativeQuery = true)
     Page<AppResponseProjection> findAllApplicationByUniversity(String universityCode, String status, Pageable pageable);
-    @Query(value = " select a.id,u.full_name fullName,k.speciality_name speciality,k.university_name university," +
+    @Query(value = " select a.id id ,u.full_name fullName,k.speciality_name speciality,k.university_name university," +
             " a.modified_date createDate from application a inner join kvota k on k.id = a.kvota_id inner join users u on u.id = a.user_id " +
-          " where k.university_code=?1 and a.application_status=?2 and (a.id::varchar ilike %?3% or u.full_name ilike %?3%) ",nativeQuery = true)
+          " where k.university_code=?1 and a.application_status=?2 and (CAST(a.id  AS varchar(255)) ilike %?3% or u.full_name ilike %?3%) ",nativeQuery = true)
     Page<AppResponseProjection> findAllSearchApplicationByUniversity(String universityCode, String status,String search, Pageable pageable);
 
     @Query("select a from Application a where a.kvota.universityCode= ?1 and a.id= ?2")
