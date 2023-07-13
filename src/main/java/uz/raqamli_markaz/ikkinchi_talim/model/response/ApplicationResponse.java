fde3cabe81ab3de3ec763c35f68e21e04d1073de
shahzod2777 <shahzodozbekov7@gmail.com>
2023-07-13 14:12:27 +0500

    фф

--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/model/response/ApplicationResponse.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/model/response/ApplicationResponse.java
@@ -17,17 +17,11 @@ public class ApplicationResponse {
 
     private Integer id;
     private String status;
-    private String fullName;
     private String message;
     private Kvota kvota;
     private DiplomaResponse diplomaResponse;
     private UserResponse userResponse;
 
     //Page uchun
-    public ApplicationResponse(Application application, User user) {
-        this.id = application.getId();
-        this.status = application.getApplicationStatus();
-        this.fullName = user.getFullName();
-    }
 
 }
