538d4c6af4eedf4b29f7f6cf45b6fa1c8a3b3209
shahzod2777 <shahzodozbekov7@gmail.com>
2023-07-26 14:54:47 +0500

    a

--- src/main/java/uz/raqamli_markaz/ikkinchi_talim/domain/Application.java
+++ src/main/java/uz/raqamli_markaz/ikkinchi_talim/domain/Application.java
@@ -15,7 +15,6 @@ public class Application extends AbstractEntity {
 
     private String applicationStatus;
     private String applicationMessage;
-    @Lob
     private String diplomaMessage;
 
     @ManyToOne
